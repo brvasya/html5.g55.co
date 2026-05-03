6edbbf2b9c1c91c8d54c9ada43ebcdc7fdea28ab
brvasya <crazygames888@gmail.com>
2026-05-03 03:40:20 +0300

    Update gameConfig.js

--- counter-craft-js/src/gameConfig.js
+++ counter-craft-js/src/gameConfig.js
@@ -14,55 +14,62 @@ import { CAVESPIDER } from "../../app/assets/enemies/caveSpider.js";
 import { CHICKENJOCKEY } from "../../app/assets/enemies/chickenJockey.js";
 
 export const GAME_CONFIG = {
-  wave: {
-    baseEnemies: 30,
-    enemiesPerWave: 3,
-    maxEnemies: 30
-  },
-    enemySpawn: {
-    types: ["zombie", "husk", "creeper", "enderman", "caveSpider", "chickenJockey"]
-  }
+wave: {
+baseEnemies: 30,
+enemiesPerWave: 3,
+maxEnemies: 30
+},
+enemySpawn: {
+types: [
+"zombie",
+"husk",
+"creeper",
+"enderman",
+"caveSpider",
+"chickenJockey"
+]
+}
 };
 
 export const GAME_ASSETS = {
-  world: {
-    map: MAP_GLB_BASE64,
-    spawnYaw: 0,
-    spawnObjectName: "G55START001",
-    floorObjectPrefixes: ["G55FLR", "G55OUT0"],
-    sky: {
-      skyColorTop: 0x6fb8ff,
-      skyColorMid: 0xa8d8ff,
-      skyColorHorizon: 0xd8f0ff,
-      fogColor: 0xd8f0ff,
-      cloudColor: 0xffffff,
-      sunColor: 0xfff4b0,
-      sunGlowColor: 0xffe7a0,
-      fogNear: 10,
-      fogFar: 100
-    }
-  },
+world: {
+map: MAP_GLB_BASE64,
+spawnYaw: 0,
+spawnObjectName: "G55START001",
+floorObjectPrefixes: ["G55FLR", "G55OUT0"],
+sky: {
+skyColorTop: 0x6fb8ff,
+skyColorMid: 0xa8d8ff,
+skyColorHorizon: 0xd8f0ff,
+fogColor: 0xd8f0ff,
+cloudColor: 0xffffff,
+sunColor: 0xfff4b0,
+sunGlowColor: 0xffe7a0,
+fogNear: 10,
+fogFar: 100
+}
+},
 
-  weaponSlots: [
-    { id: 1, asset: AK47, owned: true },
-    { id: 2, asset: P90, owned: false },
-    { id: 3, asset: AWP, owned: false },
-    { id: 4, asset: M3, owned: false },
-    { id: 5, asset: AK47, owned: false },
-    { id: 6, asset: AK47, owned: false },
-    { id: 7, asset: AK47, owned: false },
-    { id: 8, asset: AK47, owned: false },
-    { id: 9, asset: KNIFE, owned: true }
-  ],
+weaponSlots: [
+{ id: 1, asset: AK47, owned: true },
+{ id: 2, asset: P90, owned: false },
+{ id: 3, asset: AWP, owned: false },
+{ id: 4, asset: M3, owned: false },
+{ id: 5, asset: AK47, owned: false },
+{ id: 6, asset: AK47, owned: false },
+{ id: 7, asset: AK47, owned: false },
+{ id: 8, asset: AK47, owned: false },
+{ id: 9, asset: KNIFE, owned: true }
+],
 
-  enemies: {
-    types: {
-      zombie: { asset: ZOMBIE },
-      husk: { asset: HUSK },
-      creeper: { asset: CREEPER },
-      enderman: { asset: ENDERMAN },
-      caveSpider: { asset: CAVESPIDER },
-      chickenJockey: { asset: CHICKENJOCKEY },
-    }
-  }
+enemies: {
+types: {
+zombie: { asset: ZOMBIE },
+husk: { asset: HUSK },
+creeper: { asset: CREEPER },
+enderman: { asset: ENDERMAN },
+caveSpider: { asset: CAVESPIDER },
+chickenJockey: { asset: CHICKENJOCKEY }
+}
+}
 };
