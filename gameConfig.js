cfa2cb6e26997eac7b9299db163c65d800ac1dda
brvasya <crazygames888@gmail.com>
2026-07-13 06:27:19 +0300

    a

--- gameConfig.js
+++ gameConfig.js
@@ -40,16 +40,16 @@ world: WORLD,
 
 weaponSlots: [
 { id: 1, asset: PISTOL, owned: true, price: 0 },
-{ id: 2, asset: BURST_PISTOL, owned: true, price: 600 },
-{ id: 3, asset: MAGNUM, owned: true, price: 1200 },
+{ id: 2, asset: BURST_PISTOL, owned: false, price: 600 },
+{ id: 3, asset: MAGNUM, owned: false, price: 1200 },
 
 { id: 4, asset: SMG, owned: true, price: 1800 },
-{ id: 5, asset: ASSAULT_RIFLE, owned: true, price: 2400 },
-{ id: 6, asset: HEAVY_RIFLE, owned: true, price: 3000 },
+{ id: 5, asset: ASSAULT_RIFLE, owned: false, price: 2400 },
+{ id: 6, asset: HEAVY_RIFLE, owned: false, price: 3000 },
 
 { id: 7, asset: SHOTGUN, owned: true, price: 3600 },
-{ id: 8, asset: SNIPER_RIFLE, owned: true, price: 4200 },
-{ id: 9, asset: MACHINE_GUN, owned: true, price: 4800 }
+{ id: 8, asset: SNIPER_RIFLE, owned: false, price: 4200 },
+{ id: 9, asset: MACHINE_GUN, owned: false, price: 4800 }
 ],
 
 enemies: {
